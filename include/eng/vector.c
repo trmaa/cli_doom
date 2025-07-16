f54d8db60226e3d82e4e5f641e5a3d7303ea9551
trmaa <trmaayt@gmail.com>
2025-07-16 18:13:41 +0200

    optimizing suboptimal code

--- include/eng/vector.c
+++ include/eng/vector.c
@@ -7,14 +7,12 @@ eng_vec2 eng_new_vec2(float x, float y) {
     return v;
 }
 
-
 eng_ivec2 eng_new_ivec2(int x, int y) {
     eng_ivec2 v;
     v.x = x;
     v.y = y;
     return v;
 }
-
 
 eng_vec3 eng_new_vec3(float x, float y, float z) {
     eng_vec3 v;
@@ -23,7 +21,6 @@ eng_vec3 eng_new_vec3(float x, float y, float z) {
     v.z = z;
     return v;
 }
-
 
 eng_ivec3 eng_new_ivec3(int x, int y, int z) {
     eng_ivec3 v;
